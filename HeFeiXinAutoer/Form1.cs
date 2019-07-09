f20ab6be3c49ce2bed9faef7237908a1a31228d8
詹彦晶 <pettosryg@126.com>
2019-07-09 13:42:12 +0800

    fuck

--- HeFeiXinAutoer/Form1.cs
+++ HeFeiXinAutoer/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,12 @@ namespace HeFeiXinAutoer
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("fuck");
         }
     }
 }
